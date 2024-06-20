c85b16923db9cee159e74aad21838852264b7391
Basu Slathia <basuslathia01@gmail.com>
2024-06-20 12:35:54 +0530

    THIS IS THE FIST FILE
    IDR MEINE BS CODE KO RUN KRNA seekha h

---
+++ 01/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter no.";
+    cin>>a;
+    cout<<a<<ends<<"Learn Coding...";
+    return 0;
+}
+// SO THIS IS THE FIRST CODE
