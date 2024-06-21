441df9fe2b1a13413c22b8f0e2cc3d44e839dec0
Basu Slathia <basuslathia01@gmail.com>
2024-06-22 02:23:53 +0530

    Update loops vgera.cpp

--- 03/loops vgera.cpp
+++ 03/loops vgera.cpp
@@ -64,4 +64,23 @@ int main(){
     cout<<" value of a is : "<< a <<endl;
 }
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout<<"Enter the value of  a "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+
+    if(a>b) {
+        cout<<"A is greater " <<endl;
+    }
+    if(b>a){
+        cout<<"B is greater " <<endl;
+    }
+}
+ 
 
