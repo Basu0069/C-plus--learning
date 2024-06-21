a9074e928cc046a3ea6dfce0eb13b53ae95190bc
Basu Slathia <basuslathia01@gmail.com>
2024-06-22 02:05:33 +0530

    Update loops vgera.cpp

--- 03/loops vgera.cpp
+++ 03/loops vgera.cpp
@@ -20,6 +20,48 @@ int main(){
 
     cout<<"value of n is :" << n <<endl;
 }
+//  issse input lete h smja 
 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    if(a>0){
+        cout<<"A is Positive" << endl;
+    }
+    else{
+        cout <<"A is negative" << endl;
+    }
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cin>> a >>  b;
+
+    cout <<"Value of a and b is "<<a <<" "<< b <<endl;
+
+}
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    a=cin.get();
+    cout<<" value of a is : "<< a <<endl;
+}
 
 
