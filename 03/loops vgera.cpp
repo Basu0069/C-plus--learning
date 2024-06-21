5bcfbdb04c57f10f51d926223121e1fcfef7e941
Basu Slathia <basuslathia01@gmail.com>
2024-06-21 23:04:43 +0530

    loops

--- 03/loops vgera.cpp
+++ 03/loops vgera.cpp
@@ -1,1 +1,25 @@
 // Conditional pdege idr
+
+// mtlb dekh ismei pta kesi chlta h condition aise
+// if(a>0)
+// {
+//     print+ve 
+// }
+// else
+// {
+//     print(not +ve)
+// }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<"value of n is :" << n <<endl;
+}
+
+
+
