ed83160c0921c9504a50b2a4fe613ea25c0cfc05
Basu Slathia <basuslathia01@gmail.com>
2024-07-06 14:46:14 +0530

    Update fibonacci series.cpp

---
+++ 05/fibonacci series.cpp
@@ -0,0 +1,24 @@
+0,1,1,2,3,5,8,13,21
+
+n = (n-1)+(n-2)
+
+#include<iostream>
+ using namespace std;
+
+ int main(){
+    int n =10;
+    int a=0;
+    int b=1;
+    cout<<a <<" " <<b<<" ";
+    for(int i=1; i<=n;i++){
+
+        int nextNumber = a+b;
+        cout<<nextNumber<<" ";
+
+        a=b;
+        b=nextNumber;
+    }
+ }
+
+
+ 38:50  
