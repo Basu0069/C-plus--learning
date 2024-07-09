cbf4e2ad82dec4574a8975bb4617078b667edf33
Basu Slathia <basuslathia01@gmail.com>
2024-07-10 01:36:48 +0530

    Create variable & scope .cpp

---
+++ 05/variable & scope .cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 3;
+    cout << a << endl;
+
+    if(true){
+        int b = 5;
+        cout << b << endl;
+    }
+    int b=1;
+    cout << b << endl;
+
+
+
+    cout <<b << endl;
+    int i=8;
+    for(;i<8;i++){
+        cout<<" HI " << endl;
+    }
+    if(1){
+        int b;
+        if(1){
+            int b;
+            if(1){
+                cout<< b <<endl;
+            }
+        }
+    }
+    // AAP ek bde loop mei same variable ni bna sakte baar baar 
+    // infact aapko ek loop ke andar fir ek loop bnane pdegi okie 
+
+
+
+    //  OPERATOR PRecedence 
+
+
+    // QUESTION 
+    123 iska sum aur product dono nikalna h okie 
+
+
+    // NUmber of bits vala question h leetcode pe 191 
+    
+}
