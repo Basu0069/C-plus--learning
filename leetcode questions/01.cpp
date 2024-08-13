fb01cb163b8799cda6dc48cbfcbfc0458b170427
Basu Slathia <basuslathia01@gmail.com>
2024-08-13 23:21:23 +0530

    Update 01.cpp

--- leetcode questions/01.cpp
+++ leetcode questions/01.cpp
@@ -123,3 +123,42 @@ n=16
 toh divide by 2 krte jao jb k 1 na aajaye answer 
 
 
+ANSWER 
+class Solution {
+public:
+    bool isPowerOfTwo(int n){
+        
+        for(int i=0;i <= 30; i++){
+
+            int ans = pow(2,i);
+
+            if(ans ==n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+OTHER METHOD 
+class Solution {
+public:
+    bool isPowerOfTwo(int n){
+        int ans = 1;
+
+        for(int i=0;i<=30;i++){
+
+            // cout << "ans "<<ans <<endl;
+            if(ans == n){
+                return true;
+            }
+            if(ans < INT_Max/2)
+            ans = ans * 2;
+        }
+        return false;
+    }
+};
+
