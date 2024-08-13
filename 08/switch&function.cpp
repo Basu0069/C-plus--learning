da7e83bcbfbb5513708700c1fb50c96cfe1753e6
Basu Slathia <basuslathia01@gmail.com>
2024-08-14 00:07:20 +0530

    Switch&function

---
+++ 08/switch&function.cpp
@@ -0,0 +1,39 @@
+switch(expresiion){
+    case const1:    
+
+            break;
+    case const2:
+
+
+            break;
+    default: 
+
+
+}
+// const = int/char 
+const ke andar aap float aur string value ni dalsakte okie 
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num =2;
+
+    cout << endl;
+    switch (num){
+
+        case 1: cout<< "First" <<endl;
+        break;
+
+        case 2: cout<< "Second" <<endl;
+        break;
+
+        default: cout << "It is default case" <<endl;
+    }
+
+    cout << endl;
+}
