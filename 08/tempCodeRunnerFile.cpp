1678f321b403bd5b4a29baf8c3fb60e40b7176dd
Basu Slathia <basuslathia01@gmail.com>
2024-08-15 16:59:09 +0530

    Update tempCodeRunnerFile.cpp

--- 08/tempCodeRunnerFile.cpp
+++ 08/tempCodeRunnerFile.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 using namespace std;
 
-void printCounting(int n){
-    for(int i =1; i<=n;i++){
-        cout<< i << " ";
+bool  isPrime(int n){
+
+    for(int i =2; i<n;i++){
+        if(n%i==0){
+            return 0;
+        }
     }
-    cout<<endl;
+return 1;
 }
 
-int main{
 
+int main(){
     int n;
-    cin >>n;
+    cin>> n;
 
-    printCounting(n);
+    if(isPrime(n)){
+        cout<< n <<"is a prime no." <<endl;
+    }
+    else{
+        cout<< n <<"not a prime no."<<endl;
+    }
 
 
     return 0;
