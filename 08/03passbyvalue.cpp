2404a0d934aed36f33b22c3ea51923dd9de560d5
Basu Slathia <basuslathia01@gmail.com>
2024-08-15 16:57:49 +0530

    Create 03passbyvalue.cpp
    passbyvalue

---
+++ 08/03passbyvalue.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void dummy(int n){
+    n++;
+    cout << " n is " << n <<endl;
+}
+
+int main(){
+
+    int n;
+    cin>> n;
+
+    dummy(n);
+
+    cout<<" number n is "<< n << endl;
+
+    return 0;
+}
+
+
+// TOH idr pass by value 
+// ye dono n alag lag h 
+
+// dummy(n) iske andar copy bn rhi h okie 
+ 
