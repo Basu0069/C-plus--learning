fe936970a2fc97ff0897ea0989aa9cd6e973bc74
Basu Slathia <basuslathia01@gmail.com>
2024-08-18 12:43:27 +0530

    Create 02_space.cpp
    space complexity

---
+++ 11/02_space.cpp
@@ -0,0 +1,16 @@
+int a,int b = 0(1)  constant space hi leti h ye okie 
+
+
+
+eg- 
+int arr[5]={1,2,3,4,5}   
+SC- > 0(1)
+
+
+
+
+eg- int n;
+cin>>n;
+vector<int> v(n);
+lenght n h toh 
+space complexity -> O(n)
