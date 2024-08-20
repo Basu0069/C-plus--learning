63d76e0fb3e50ff9e3cd3675515fde96406f497d
Basu Slathia <basuslathia01@gmail.com>
2024-08-20 17:57:21 +0530

    Update pattern.cpp

--- 03/pattern.cpp
+++ 03/pattern.cpp
@@ -24,6 +24,25 @@ int main(){
 
 
 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+
+
 
 
 #include<iostream>
