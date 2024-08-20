9055ec409e05d8fcb3e099e7cb9a98604e312c62
Basu Slathia <basuslathia01@gmail.com>
2024-08-20 17:57:16 +0530

    Update loops vgera.cpp

--- 03/loops vgera.cpp
+++ 03/loops vgera.cpp
@@ -184,23 +184,24 @@ Farheit to celsius convert
 Prime btana h ki no. prime h ya ni vo vala question kr
 #include<iostream>
 using namespace std;
-
 int main(){
-
+    
     int n;
     cin>>n;
-
+    
     int i=2;
-
+    
     while(i<n){
         if(n%i==0){
-            cout<< "Not prime for "<< i << endl;
+            cout<<"IT is not prime"<<endl;
+            break;
         }
         else{
-            cout<<"Prime for "<< i<<endl;
+            cout<<"IT is prime "<<endl;
+            break;
         }
-        i=i+1;
-
+    i=i+1;
+    
     }
 }
 
