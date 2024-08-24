cccb2a302f60af3804324af74a759b1a94f184af
Basu Slathia <basuslathia01@gmail.com>
2024-08-24 23:26:16 +0530

    Update 01_binarysearch.cpp

--- 12/01_binarysearch.cpp
+++ 12/01_binarysearch.cpp
@@ -95,3 +95,72 @@ not found
 
 CODE :
  
+ #include<iostream>
+ using namespace std;
+
+ int binarySearch(int arr[], int size, int key){
+
+    int start = 0;
+    int end=size -1;
+
+    int mid = start + (end-start)/2;
+
+    while(start <= end){
+
+        if(arr[mid] == key){
+            return mid;
+        }
+
+        // go to right wala part 
+        if(key > arr[mid]){
+            start = mid + 1 ;
+        }
+        else{
+            end = mid -1;
+        }
+        mid= start + (end-start)/2;
+    }
+    return -1;
+ }
+
+
+
+int main(){
+
+    int even[6]={2,4,6,8,12,18};
+    int odd[5]={3,8,11,14,16};
+
+    int evenindex = binarySearch(even, 6, 12);
+
+    cout << "Index of 12 is "<< evenindex << endl;
+
+    int oddindex = binarySearch(odd, 5, 8);
+
+    cout<< " Index of 8 is " << oddindex << endl;
+    return 0;
+} 
+
+
+// mid=(s+e)/2 
+// u can write s+(e-s)/2 
+this is another form of representation in order to remove the partiality of big numbers ;
+ 
+
+// linear search meo 1000 values 
+wost case -   1000 comparsions hoga 
+complexity O(n);
+
+Binary search mie 
+1000 value h toh 
+phele 500  fir 250 fir 125 fir 62 fir 31 fir 15 fir 7 fir 3 fir 1 fir 0 sirf 10 com
+parison mei ho jayega sei b.s mei 
+IDR complexity h O(log n)
+  
+
+  Binary searh mie 
+  phel N fir N/2 size fir N/4 fir N/8 fir ---- N/2k 
+  mtlb ki 
+  N/2^k =1 
+  N=2^k
+  k=log N 
+   
