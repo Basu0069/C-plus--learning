c0ac3aa4c72c5a68a1cd9050fa8614e84de8e214
Basu Slathia <basuslathia01@gmail.com>
2024-09-11 18:12:17 +0530

    Create 06_queue.cpp

---
+++ 19/06_queue.cpp
@@ -0,0 +1,23 @@
+// first in first out 
+   
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main(){
+
+    queue<string>q;
+
+    q.push("love");
+    q.push("babbar");
+    q.push("kumar");
+
+    cout<<"Size before pop"<<q.size()<<endl;
+
+    cout<<"First Element "<<q.front()<<endl;
+    q.pop();
+    cout<<"First Element "<<q.front()<<endl;
+
+    cout<<"Size after pop"<<q.size()<<endl;
+
+}
