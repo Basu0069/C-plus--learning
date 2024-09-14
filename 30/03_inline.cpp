9fff16842bee991e36d4592e3486eb258c101f19
Basu Slathia <basuslathia01@gmail.com>
2024-09-14 23:33:26 +0530

    Create 03_inline.cpp

---
+++ 30/03_inline.cpp
@@ -0,0 +1,33 @@
+// inline function
+
+// used to reduce fucntion call overhead 
+
+ #include<iostream>
+ using namespace std;
+
+
+ inline int getMax(int& a, int& b){
+    return (a>b)? a: b;
+ }
+
+ int main(){
+
+    int a =1, b=2;
+    int ans=0;
+
+    ans = getMax(a,b);
+    cout << ans<<endl;
+
+    a= a+3;
+    b= b+1;
+
+    ans = getMax(a,b);
+    cout << ans << endl;
+
+    return 0;
+ }
+
+//  inline function kya krta h - 
+agr u hv inly only line in a function tb compililer man jayega inline function bnane ke liye isse 
+
+ye cheez phele hi compile hone se phele hi udr aajayegi likhi hui ok 
