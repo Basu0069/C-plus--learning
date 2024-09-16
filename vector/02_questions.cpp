e0da8940efd57ea2c85e4a7b3b485cf3c61950f0
Basu Slathia <basuslathia01@gmail.com>
2024-09-16 19:04:45 +0530

    Update 02_questions.cpp

--- vector/02_questions.cpp
+++ vector/02_questions.cpp
@@ -28,7 +28,7 @@ int main(){
     // }
 
     // second method 
-    for(int i=v.size()-1;i>=0;i++){
+    for(int i=v.size()-1;i>=0;i--){
         if(v[i]==x){
             occurence=i;
             break; 
