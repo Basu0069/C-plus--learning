c137c93eba0f50d509a7ffada5d3c985c7f0504c
Basu Slathia <basuslathia01@gmail.com>
2024-09-19 19:36:25 +0530

    Create Q25.cpp

---
+++ vector/Q25.cpp
@@ -0,0 +1,30 @@
+
+the first line of input consist of an integer n representing the items to be inserted at the begining of the linked list 
+
+the second line of input considst of an representing the hcoice or stop the insertion process 
+
+Input:
+2
+0
+4
+1
+OUtput: 
+
+Node inserted
+Node inserted
+Linked List: 4 2 
+Node ended
+
+
+
+INPUT:
+4
+1
+
+OUTPUT:
+Node inserted
+Linked List: 4 
+Node ended
+
+
+
