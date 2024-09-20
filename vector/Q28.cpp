eae800df983d753354192024eef3586960863ad5
Basu Slathia <basuslathia01@gmail.com>
2024-09-20 22:53:54 +0530

    Create Q28.cpp

---
+++ vector/Q28.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int game;
+    int n;
+    cin>>n;
+    int value=1;
+    int every=1;
+    int pass,c;
+    while(value<n){
+        c=every*2;
+        pass=c;
+        every=pass;
+    }
+    cout<<every<<endl;
+    
+}
