60d647be400bd33fc6a3fd7d3205b06f5e058a75
Basu Slathia <basuslathia01@gmail.com>
2024-11-24 19:25:00 +0530

    Create 04.cpp

---
+++ heap/04.cpp
@@ -0,0 +1,34 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+
+    vector<int>a;
+
+    for(int i=0;i<n;i++){
+        int element;
+        cin>>element;
+        a.push_back(element);
+    make_heap(a.begin(),a.end());
+    }
+cout<<endl;
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+cout<<endl;
+
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]*a[i]*a[i]<<" ";
+    }
+
+
+
+
+
+
+
+
+}
